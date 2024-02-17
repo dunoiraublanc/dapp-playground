524487956a1ce3a4c0842219f326c1ebed37b512
dunoiraublanc <50521836+dunoiraublanc@users.noreply.github.com>
2024-02-17 04:37:16 +0000

    add partisia2

---
+++ zk-struct-open/src/zk_compute.rs
@@ -0,0 +1,25 @@
+use create_type_spec_derive::CreateTypeSpec;
+use pbc_zk::*;
+
+#[allow(unused)]
+#[derive(pbc_zk::SecretBinary, Clone, CreateTypeSpec)]
+pub struct SecretPosition {
+    x: Sbi8,
+    y: Sbi8,
+}
+
+#[allow(unused)]
+#[derive(pbc_zk::SecretBinary, Clone, CreateTypeSpec)]
+pub struct SecretResponse {
+    age: Sbi8,
+    height: Sbi16,
+    position: SecretPosition,
+    wealth: Sbi128,
+}
+
+#[zk_compute(shortname = 0x61)]
+pub fn open_but_first_add_300(input_id: SecretVarId) -> SecretResponse {
+    let mut value = load_sbi::<SecretResponse>(input_id);
+    value.wealth = value.wealth + Sbi128::from(300i128);
+    value
+}
